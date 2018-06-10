8541a9afde3f98e9685fa9407686a7403f21d95f
Mattie Terzolo <mattie.terzolo@gmail.com>
2018-06-10 15:58:25 -0700

    Create dumb baseline script

---
+++ bin/dumb_baseline.py
@@ -0,0 +1,12 @@
+import pickle
+import numpy as np
+from sklearn.metrics import mean_squared_error
+
+with open('data//annotation_training.pkl', 'rb') as f:
+    annotation_training = pickle.load(f, encoding='latin1')
+
+y_train = np.fromiter(annotation_training['interview'].values(), dtype=float)
+y_preds = np.full((len(y_train),), np.mean(y_train))
+
+print(np.sqrt(mean_squared_error(y_train, y_preds)))
+print(mean_squared_error(y_train, y_preds))
