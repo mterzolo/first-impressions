3047ba8eb67b2b1b7f754093530f18212d5f0c87
Mattie Terzolo <mattie.terzolo@gmail.com>
2018-06-11 14:14:17 -0700

    Modularize by adding transform step to lib file as a function

--- bin/image_model_2.py
+++ bin/image_model_2.py
@@ -1,9 +1,5 @@
-import os
-import keras
+
 import pickle
-import numpy as np
-from keras.preprocessing.image import load_img
-from keras.preprocessing.image import img_to_array
 from keras.applications import vgg16
 from keras.models import Model
 from keras.layers import Dense, Input
@@ -12,37 +8,10 @@ from keras.layers.recurrent import LSTM
 from keras.layers.wrappers import TimeDistributed
 from keras.optimizers import Nadam
 
-# Open answers file
-with open('../data/meta_data/annotation_training.pkl', 'rb') as f:
-    annotation_training = pickle.load(f, encoding='latin1')
+import lib
 
-# Get all IDs for videos for the training set
-vid_ids = os.listdir('../data/image_data/training_data')[0:10]
-y_train = [annotation_training['interview'][i + '.mp4'] for i in vid_ids]
-
-# Create empty array to store image data
-X_train = np.empty(shape=(0, 100, 224, 224, 3))
-
-for video in vid_ids:
-
-    images = os.listdir('../data/image_data/training_data/{}'.format(video))
-    X_temp = np.empty(shape=(0, 224, 224, 3))
-
-    for image in images:
-        # Load the image
-        filename = '../data/image_data/training_data/{}/frame50.jpg'.format(video)
-        original = load_img(filename, target_size=(224, 224))
-
-        # Convert to numpy array
-        numpy_image = img_to_array(original)
-
-        # Resize and store in one big array
-        image_temp = np.expand_dims(numpy_image, axis=0)
-        image_temp = vgg16.preprocess_input(image_temp)
-        X_temp = np.concatenate((X_temp, image_temp), axis=0)
-
-    X_temp = np.expand_dims(X_temp, axis=0)
-    X_train = np.concatenate((X_train, X_temp), axis=0)
+X_train, y_train = lib.img2array3D(data_split='training', num_samples=500, num_frames=100)
+X_test, y_test = lib.img2array3D(data_split='test', num_samples=100, num_frames=100)
 
 video = Input(shape=(100, 224, 224, 3))
 
@@ -72,3 +41,8 @@ model.compile(loss="mean_squared_error",
 # Create the model
 model.compile(optimizer='Adam', loss='mean_squared_error')
 model.fit(X_train, y_train, epochs=10, batch_size=32)
+
+# Save model
+pkl_filename = '../output/image_model_2.pkl'
+with open(pkl_filename, 'wb') as file:
+    pickle.dump(model, file)
--- bin/lib.py
+++ bin/lib.py
@@ -37,3 +37,48 @@ def img2array(data_split, num_samples, frame_num):
         counter += 1
 
     return X, y
+
+
+def img2array3D(data_split, num_samples, num_frames):
+
+    # Open answers file
+    with open('../data/meta_data/annotation_{}.pkl'.format(data_split), 'rb') as f:
+        label_file = pickle.load(f, encoding='latin1')
+
+    # Get all IDs for videos for the training set
+    vid_ids = os.listdir('../data/image_data/{}_data'.format(data_split))[0:num_samples]
+    y = [label_file['interview'][i + '.mp4'] for i in vid_ids]
+
+    # Create empty array to store image data
+    X = np.empty(shape=(num_samples, 100, 224, 224, 3))
+    out_counter = 0
+
+    for video in vid_ids:
+
+        images = os.listdir('../data/image_data/{}_data/{}'.format(data_split, video))
+        X_temp = np.zeros(shape=(num_frames, 224, 224, 3))
+        in_counter = 0
+
+        for image in images:
+
+            # Load the image
+            original = load_img(image, target_size=(224, 224))
+
+            # Convert to numpy array
+            numpy_image = img_to_array(original)
+
+            # Resize and store in one big array
+            image_temp = np.expand_dims(numpy_image, axis=0)
+            image_temp = vgg16.preprocess_input(image_temp)
+            X_temp[in_counter] = image_temp
+
+            # Increment counter for number of images in observation
+            in_counter += 1
+
+        X_temp = np.expand_dims(X_temp, axis=0)
+        X[out_counter] = X_temp
+
+        # Increment counter for observations in dataset
+        out_counter += 1
+
+    return X, y
