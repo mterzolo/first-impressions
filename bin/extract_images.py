21a41096cbea01d36228a4ec9443cda29c4ba2a4
Mattie Terzolo <mattie.terzolo@gmail.com>
2018-06-10 18:33:08 -0700

    Update file paths to represent new folder structure

--- bin/extract_images.py
+++ bin/extract_images.py
@@ -2,25 +2,25 @@ import cv2
 import os
 
 partition = 'test'
-file_chunks = os.listdir('unzippedData')
+file_chunks = os.listdir('../data/video_data')
 file_chunks = [i for i in file_chunks if partition in i]
 
 for chunk in file_chunks:
 
-    files = os.listdir('unzippedData/{}'.format(chunk))
+    files = os.listdir('../data/video_data/{}'.format(chunk))
 
     for file_name in files:
 
         # Create video object
-        cap = cv2.VideoCapture('unzippedData/{}/{}'.format(chunk, file_name))
+        cap = cv2.VideoCapture('../data/video_data/{}/{}'.format(chunk, file_name))
 
         # Get file name
         file_name = (file_name.split('.mp4'))[0]
 
         # Create new folder for images
         try:
-            if not os.path.exists('ImageData/{}Data/{}'.format(partition, file_name)):
-                os.makedirs('ImageData/{}Data/{}'.format(partition, file_name))
+            if not os.path.exists('image_data/{}_data/{}'.format(partition, file_name)):
+                os.makedirs('image_data/{}_data/{}'.format(partition, file_name))
 
         except OSError:
             print('Error: Creating directory of data')
@@ -48,7 +48,7 @@ for chunk in file_chunks:
             frame = cv2.resize(frame, (224, 224), interpolation=cv2.INTER_CUBIC)
 
             # Save image to jpg
-            name = 'ImageData/{}Data/{}/frame{}.jpg'.format(partition, file_name, count)
+            name = 'image_data/{}_data/{}/frame{}.jpg'.format(partition, file_name, count)
             cv2.imwrite(name, frame)
 
             if cv2.waitKey(1) & 0xFF == ord('q'):
