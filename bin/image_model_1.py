1f0bf14b8ce1edca2d7275da812fc0ddb4cf63c4
Mattie Terzolo <mattie.terzolo@gmail.com>
2018-06-11 22:30:02 -0700

    Add additional trainable layers and increase dropout to .4

--- bin/image_model_1.py
+++ bin/image_model_1.py
@@ -2,7 +2,7 @@ import pickle
 import keras
 from keras.applications import vgg16
 from keras.models import Model
-from keras.layers import Dense, Flatten
+from keras.layers import Dense, Flatten, Dropout
 
 import lib
 
@@ -14,9 +14,15 @@ X_test, y_test = lib.img2array(data_split='test', num_samples=2000, frame_num=10
 base_model = vgg16.VGG16(weights="imagenet", include_top=False, input_shape=(224, 224, 3))
 x = base_model.output
 
-# Create the last two additional layers
+# Create additional layers
 x = Flatten()(x)
 x = Dense(1024, activation='relu')(x)
+x = Dense(512, activation='relu')(x)
+x = Dropout(.4)(x)
+x = Dense(512, activation='relu')(x)
+x = Dropout(.4)(x)
+x = Dense(256, activation='relu')(x)
+x = Dropout(.4)(x)
 preds = Dense(1, activation='linear')(x)
 
 # Freeze all original layers
@@ -24,14 +30,17 @@ for layer in base_model.layers:
     layer.trainable = False
 
 # Optimizer
-optimizer = keras.optimizers.Adam(lr=0.001, decay=0.001)
+optimizer = keras.optimizers.Adam(lr=0.001, decay=0.0006)
+
+# fit model
+filename = '../output/image_model.h5'
+checkpoint = ModelCheckpoint(filename, monitor='val_loss', verbose=1, save_best_only=True, mode='min')
 
 # Create the model
 model = Model(base_model.input, preds)
 model.compile(optimizer=optimizer, loss='mean_squared_error')
-model.fit(X_train, y_train, validation_data=(X_test, y_test), epochs=100, batch_size=32)
-
-# Save model
-pkl_filename = '../output/image_model_1.pkl'
-with open(pkl_filename, 'wb') as file:
-    pickle.dump(model, file)
+model.fit(X_train, y_train,
+          validation_data=(X_test, y_test),
+          epochs=150,
+          batch_size=32,
+          callbacks=[checkpoint])
