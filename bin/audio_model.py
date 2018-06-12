bee196cc5e89fdac16831e01b1f1ec4a954f13ac
Mattie Terzolo <mattie.terzolo@gmail.com>
2018-06-12 10:28:14 -0700

    Save model to output folder, update on valdation loss only

--- bin/audio_model.py
+++ bin/audio_model.py
@@ -1,11 +1,12 @@
 from keras.layers import Dense, Flatten
 from keras.layers import Conv2D, MaxPooling2D
 from keras.models import Sequential
+from keras.callbacks import ModelCheckpoint
 
 import lib
 
-X_train, y_train = lib.audio2melspec(data_split='training', num_samples=500)
-X_test, y_test = lib.audio2melspec(data_split='test', num_samples=500)
+X_train, y_train = lib.audio2melspec(data_split='training', num_samples=6000)
+X_test, y_test = lib.audio2melspec(data_split='test', num_samples=2000)
 
 X_train = X_train.reshape(X_train.shape[0], 128, 662, 1)
 X_test = X_test.reshape(X_test.shape[0], 128, 662, 1)
@@ -23,10 +24,13 @@ model.add(Flatten())
 model.add(Dense(1024, activation='relu'))
 model.add(Dense(1, activation='linear'))
 
+# fit model
+filename = '../output/audio_model.h5'
+checkpoint = ModelCheckpoint(filename, monitor='val_loss', verbose=1, save_best_only=True, mode='min')
 model.compile(optimizer='Adam', loss='mean_squared_error')
-model.fit(X_train, y_train, validation_data=(X_test, y_test), epochs=100, batch_size=32)
+model.fit(X_train, y_train,
+          validation_data=(X_test, y_test),
+          epochs=100,
+          batch_size=32,
+          callbacks=[checkpoint])
 
-# Save model
-pkl_filename = '../output/audio_model.pkl'
-with open(pkl_filename, 'wb') as file:
-    pickle.dump(model, file)
