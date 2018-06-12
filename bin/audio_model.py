3cb1de7082f3d3e5708dba1d043dc06294b5f1e1
Mattie Terzolo <mattie.terzolo@gmail.com>
2018-06-12 10:21:13 -0700

    Update transform step to normalize all MFCC arrays to same size

--- bin/audio_model.py
+++ bin/audio_model.py
@@ -4,8 +4,8 @@ from keras.models import Sequential
 
 import lib
 
-X_train, y_train = lib.audio2melspec(data_split='training', num_samples=5)
-X_test, y_test = lib.audio2melspec(data_split='test', num_samples=5)
+X_train, y_train = lib.audio2melspec(data_split='training', num_samples=500)
+X_test, y_test = lib.audio2melspec(data_split='test', num_samples=500)
 
 X_train = X_train.reshape(X_train.shape[0], 128, 662, 1)
 X_test = X_test.reshape(X_test.shape[0], 128, 662, 1)
@@ -16,12 +16,11 @@ model = Sequential()
 model.add(Conv2D(32, kernel_size=(5, 5), strides=(1, 1),
                  activation='relu',
                  input_shape=input_shape))
-
 model.add(MaxPooling2D(pool_size=(2, 2), strides=(2, 2)))
 model.add(Conv2D(64, (5, 5), activation='relu'))
 model.add(MaxPooling2D(pool_size=(2, 2)))
 model.add(Flatten())
-model.add(Dense(1000, activation='relu'))
+model.add(Dense(1024, activation='relu'))
 model.add(Dense(1, activation='linear'))
 
 model.compile(optimizer='Adam', loss='mean_squared_error')
